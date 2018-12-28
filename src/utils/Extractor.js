81076b688364a79a3dfbbddede02035a560dcd26
wangdy <wangdy123321>
2018-12-28 15:46:06 +0800

    added by chris

--- src/utils/Extractor.js
+++ src/utils/Extractor.js
@@ -12,13 +12,13 @@ export default {
     var matchedLocationResult = lineString.match(/__Location:.*?#/g)
     var matchedTimeResult = lineString.match(/__Time:.*?#/g)
     var providerResult = lineString.match(/__Provider:.*?#/g)
-
-    if (lineString === undefined || lineString === null || matchedLocationResult == null || matchedTimeResult == null || providerResult == null) {
+    var loc, tm, provider
+    if (lineString === undefined || lineString === null || matchedLocationResult == null) {
       return null
     }
-    var loc = matchedLocationResult[0].replace('__Location:', '').replace('#', '').split(',')
-    var tm = matchedTimeResult[0].replace('__Time:', '').replace('#', '')
-    var provider = providerResult[0].replace('__Provider:', '').replace('#', '')
+    loc = matchedLocationResult[0].replace('__Location:', '').replace('#', '').split(',')
+    if (matchedTimeResult !== null) { tm = matchedTimeResult[0].replace('__Time:', '').replace('#', '') }
+    if (providerResult !== null) { provider = providerResult[0].replace('__Provider:', '').replace('#', '') }
 
     return {
       lng: loc[0],
